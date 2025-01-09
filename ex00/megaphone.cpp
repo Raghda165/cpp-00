61a26bc9b8e6a84c0f2f27046202e474ad8eb783
Ragda Khidir <125825233+Raghda165@users.noreply.github.com>
2025-01-09 05:52:00 +0000

    m

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -18,16 +18,18 @@ int main(int ac ,char **av)
 	char c;
 	j = 1;
 	if (ac == 1)
-		std::cout <<"* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+		std::cout <<"* LOUD AND UNBEARABLE FEEDBACK NOISE *"<<std::endl;
 	while (j != ac)
 	{
 		i = 0;
 		while (av[j][i])
 		{
-			c = std::toupper(av[j][i]);
+			if((av[j][i]>= 65 && av[j][i]<= 90 )||(av[j][i]>= 97 && av[j][i]<= 122) )
+				{c = std::toupper(av[j][i]);}
 			std::cout<< c ;
 			i++;
 		}
 		j++;
 	}
+	std::cout<<std::endl;
 }
